fcf271120cfa1cfbd7fb908952ece4b9d195c831
Md. Shoriful Islam <shorifulislam776@gmail.com>
2026-04-09 21:44:12 +0600

    MVC Project

---
+++ ITC.HRIS.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITC.HRIS.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
