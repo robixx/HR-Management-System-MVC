2399cd4b089e1ca89603ccb45b0c14ff6c9b8c26
Md. Shoriful Islam <shorifulislam776@gmail.com>
2026-05-08 20:13:42 +0600

    addressbook ui currection

---
+++ ITC.HRIS.WEB/Areas/Admin/Controllers/VisitsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITC.HRIS.WEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class VisitsController : Controller
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> AddressBook()
+        {
+            return View();
+        }
+    }
+}
