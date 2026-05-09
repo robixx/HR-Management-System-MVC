37a6c1d73504c2ae7024968fc837a8920b187a2e
Md. Shoriful Islam <shorifulislam776@gmail.com>
2026-05-09 20:03:32 +0600

    hrhandbook ui design

--- ITC.HRIS.WEB/Areas/Admin/Controllers/VisitsController.cs
+++ ITC.HRIS.WEB/Areas/Admin/Controllers/VisitsController.cs
@@ -13,5 +13,12 @@ namespace ITC.HRIS.WEB.Areas.Admin.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> HRHandBook()
+        {
+            return View();
+        }
     }
 }
