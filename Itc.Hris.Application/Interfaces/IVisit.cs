280648f20249d3ee09c8c92685b8f2929d1096b0
Md. Shoriful Islam <shorifulislam776@gmail.com>
2026-05-09 20:06:51 +0600

    hrhandbook and addressbook service

---
+++ Itc.Hris.Application/Interfaces/IVisit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itc.Hris.Application.Interfaces
+{
+    public interface IVisit
+    {
+        Task<(string Message, bool Status)> AddressBook();
+    }
+}
---
+++ Itc.Hris.Infrastructure/Services/VisitService.cs
@@ -0,0 +1,18 @@
+﻿using Itc.Hris.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itc.Hris.Infrastructure.Services
+{
+    public class VisitService : IVisit
+    {
+        public async Task<(string Message, bool Status)> AddressBook()
+        {
+            // Implementation for AddressBook method
+            return ("AddressBook method not implemented", false);
+        }
+    }
+}
