f08b633c19a993d7507133da1112551629fb61c0
Md. Shoriful Islam <shorifulislam776@gmail.com>
2026-05-15 21:28:17 +0600

    leave apply

---
+++ ITC.HRIS.WEB/Areas/Admin/Controllers/LeaveApplicationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITC.HRIS.WEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class LeaveApplicationController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult LeaveCreate()
+        {
+            return View();
+        }
+    }
+}
